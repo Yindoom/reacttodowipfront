26cbde36254b9cb7b5ac09d1fb8f4d4b82085b7e
Yindo <bast0099@easv365.dk>
2020-09-22 09:53:44 +0100

    Added cards and clicking on card to make task done

--- src/Components/todo.tsx
+++ src/Components/todo.tsx
@@ -11,27 +11,40 @@ export class TodoComponent extends React.Component<{
     render() {
 
         const todo = this.props.todo;
+        const date = new Intl.DateTimeFormat("en-GB", {
+            year: "numeric",
+            month: "long",
+            day: "2-digit",
+            hour: "numeric",
+            minute: "numeric"
+        }).format(new Date(todo.deadline));
+        const today = new Date();
+        const todoDay = new Date(date);
+        const isToday = today.getDate() === todoDay.getDate();
+        const isLate = today.getDate() >= todoDay.getDate();
         return (
-            <Card border="primary" style={{ width: '18rem' }}>
+            <Card border="secondary" style={{ width: '18rem' }} onClick={ () => this.updateTodo() } >
                 <Card.Body>
-                    <Card.Title>{todo.deadline}</Card.Title>
+                    <Card.Title>{date}</Card.Title>
                     <Card.Text>
                         {todo.description}
                     </Card.Text>
-                    <input type="checkbox" checked={todo.isDone} onChange={(event) => this.updateTodo(event.target)} />
+                    <input type="checkbox" checked={todo.isDone}/>
                 </Card.Body>
+                {isToday && !todo.isDone && <Card.Footer>This task is due today.</Card.Footer>}
+                {isLate && !todo.isDone && <Card.Footer>This task is late</Card.Footer>}
             </Card>
-            
+
         );
     }
 
-    updateTodo(check: HTMLInputElement) {
+    updateTodo() {
         const todo = this.props.todo;
         const update: Todo = {
             deadline: todo.deadline,
             description: todo.description,
             id: todo.id,
-            isDone: check.checked
+            isDone: !todo.isDone
         }
         this.props.updateTodo(update)
     }
